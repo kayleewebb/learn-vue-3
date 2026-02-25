6e4189baba965d849791296d0884bc0e35a04709
Kaylee Webb <kayleewebb@MacBook-Pro-2.local>
2026-02-25 12:50:31 -0600

    Conditional rendering

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,28 @@
 <template>
-  <h1>v-model vs v-bind</h1>
-  <h2>message: {{ message }}</h2>
-  <div>
-    <p>v-model = v-bind + @input</p>
-    <input
-      type="text"
-      v-bind:value="message"
-      @input="message = $event.target.value"
-    />
-  </div>
-  <div>
-    <p>v-model</p>
-    <input type="text" v-model="message" />
-  </div>
+  <h1>{{ message }}</h1>
+  <p>
+    What has keys but can't open locks, space but no room, and you can enter but
+    can't go outside? What am I?
+  </p>
+
+  <button style="width: 200px" @click="showAnswer = !showAnswer">
+    {{ !showAnswer ? 'Show me the answer!' : 'Hide the answer!' }}
+  </button>
+
+  <div v-if="showAnswer">A keyboard.</div>
+
+  <div v-show="showAnswer">A keyboard.</div>
+
+  <!-- <div v-if="Math.random() > 0.5">Now you see me</div>
+  <div v-else>Now you don't</div> -->
 </template>
 
 <script setup>
 import { ref } from 'vue'
 
-let message = ref('Hello!')
+let message = ref('Hello, v-if!')
+
+let showAnswer = ref(false)
 </script>
 
 <style scoped></style>
