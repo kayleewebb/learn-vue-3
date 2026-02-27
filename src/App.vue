a20e7476e489d74bc102500da0d13691bc74a500
Kaylee Webb <kayleewebb@MacBook-Pro-2.local>
2026-02-27 12:11:54 -0600

    array change detection

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,37 @@
 <template>
   <h1>{{ message }}</h1>
   <button @click="sortUsersByAge">Sort users by age</button>
+  <button @click="hideInactiveUsers">Hide inactive users</button>
+  <button @click="showFirstTwoUsers">Show first two users</button>
   <ul>
     <li v-for="(user, index) in users" :key="user.id">
       {{ index }} - {{ user.id }} - {{ user.name }} - {{ user.age }} -
       {{ user.isActive }}
     </li>
   </ul>
-
-  <hr />
-
-  <table>
-    <tr>
-      <th>Index</th>
-      <th>Id</th>
-      <th>Name</th>
-      <th>Age</th>
-      <th>Operation</th>
-    </tr>
-    <tr v-for="(user, index) in users" :key="user.id">
-      <td>{{ index }}</td>
-      <td>{{ user.id }}</td>
-      <td :class="{ inactive: !user.isActive }">{{ user.name }}</td>
-      <td>{{ user.age }}</td>
-      <td>
-        <button @click="user.isActive = !user.isActive">
-          {{ user.isActive ? 'Deactivate' : 'Restore' }}
-        </button>
-      </td>
-    </tr>
-  </table>
-
-  <hr />
-  <p>Use v-for to iterate through the properties of an object:</p>
-  <div v-for="(value, key, index) in users[0]">
-    {{ index }} - {{ key }}: {{ value }}
-  </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 
-let message = ref('Hello, v-for!')
+let message = ref('Hello, Array Change Detection!')
 
 const users = ref([
-  { id: 1001, name: 'John Smith', age: 26, isActive: true },
-  { id: 1002, name: 'Tom Doe', age: 16, isActive: true },
+  { id: 1001, name: 'John Smith', age: 26, isActive: false },
+  { id: 1002, name: 'Tom Doe', age: 16, isActive: false },
   { id: 1003, name: 'Frankin Wong', age: 18, isActive: true }
 ])
 
 function sortUsersByAge() {
   users.value.sort((a, b) => a.age - b.age)
+}
+
+function hideInactiveUsers() {
+  users.value = users.value.filter((user) => user.isActive)
+}
+
+function showFirstTwoUsers() {
+  users.value = users.value.slice(0, 2)
 }
 </script>
 
