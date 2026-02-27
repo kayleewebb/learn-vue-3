9701c872fef378a978ed009991bd091566accc52
Kaylee Webb <kayleewebb@MacBook-Pro-2.local>
2026-02-27 13:15:03 -0600

    lifecycle hooks

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,60 @@
 <template>
+  <h1>{{ message }}</h1>
   <div class="card">
-    <h1 ref="title">Hello, Template Refs!</h1>
-    <input type="text" ref="input" />
-    <button @click="printDomElements">Print DOM elements in console log</button>
-    <button @click="changeTitle">Change title</button>
+    <h2 ref="title">This is the App component.</h2>
+    <h2>Number: {{ number }}</h2>
+    <button @click="number++">Increment number by one</button>
+    <button @click="isShow = !isShow">Toggle component1</button>
+    <Component1 v-if="isShow"></Component1>
   </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
+import {
+  ref,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+  watch
+} from 'vue'
 
-let title = ref()
-const input = ref()
+import Component1 from './Component1.vue'
 
-function printDomElements() {
+let message = ref('Hello, Lifecycle Hooks!')
+
+let number = ref(1)
+let title = ref(null)
+
+let isShow = ref(true)
+
+console.log('App component is setup.')
+
+onBeforeMount(() => {
+  console.log('App component is before mount.')
+  console.log(number.value)
   console.log(title.value)
-  console.log(input.value)
-}
-
-function changeTitle() {
-  title.value.innerText = 'Hello world!'
-}
-
+})
 onMounted(() => {
-  input.value.focus() 
+  console.log('App component is mounted.')
+  console.log(title.value)
+})
+onBeforeUpdate(() => {
+  console.log('App component is before update.')
+})
+onUpdated(() => {
+  console.log('App component is updated.')
+})
+onBeforeUnmount(() => {
+  console.log('App component is before unmount.')
+})
+onUnmounted(() => {
+  console.log('App component is unmounted.')
+})
+watch(number, () => {
+  console.log('number changes!')
 })
 </script>
 
