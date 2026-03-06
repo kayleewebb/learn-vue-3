3fe73d67d6b7585496617d805245e71ef8ed7114
Kaylee Webb <kayleewebb@MacBook-Pro-2.local>
2026-03-06 16:33:23 -0600

    component events

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,15 @@
 <template>
-  <div style="background-color: orange; padding: 5px">
-
-    <div v-for="post in posts" :key="post.id">
-      <BlogPost
-        :id="post.id"
-        :blog-post-title="post.blogPostTitle"
-        :blog-post-content="post.blogPostContent"
-      />
-
-      <button @click="post.blogPostTitle = 'A different title'">
-        Change blog post title
-      </button>
-
-      <hr />
-    </div>
-
-  </div>
+  <BlogPost
+    v-for="post in posts"
+    :key="post.id"
+    v-bind="post"
+    @delete-blog-post="processDeletion"
+  ></BlogPost>
 </template>
 
 <script setup>
-import BlogPost from '@/BlogPost.vue'
 import { ref } from 'vue'
-
-let blogPostId = ref(2)
-let blogPostTitle = ref(
-  'HP and the Cursed Child Broadway production suspended until April'
-)
-let blogPostContent = ref(
-  'Adding to the earlier post, the Broadway production of Harry Potter and the Cursed Child has been...'
-)
-
+import BlogPost from './BlogPost.vue'
 let posts = ref([
   {
     id: 1,
@@ -57,6 +37,12 @@ let posts = ref([
       'It’s no secret that the Harry Potter series is a global phenomenon, having been translated into over 80 languages to date....'
   }
 ])
+
+// The second argument from the BlogPost’s emit method will be passed as the first parameter of this method.
+function processDeletion(id) {
+  let index = posts.value.findIndex((item) => item.id == id)
+  posts.value.splice(index, 1)
+}
 </script>
 
 <style scoped></style>
