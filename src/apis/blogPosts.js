e8fdffc0492210927728e14b3c6408dda374fa33
Kaylee Webb <kayleewebb@MacBook-Pro-2.local>
2026-04-06 12:19:24 -0500

    Named views

--- src/apis/blogPosts.js
+++ src/apis/blogPosts.js
@@ -1,4 +1,9 @@
 const BASE_URL = 'http://localhost:3000/blogPosts'
+
+/**
+ * Fetch all blog posts from the API
+ * @returns {Promise<Array>} A promise that resolves to an array of blog posts
+ */
 const findAll = async () => {
   try {
     const response = await fetch(BASE_URL)
@@ -8,11 +13,15 @@ const findAll = async () => {
     return await response.json()
   } catch (error) {
     console.error(error)
-    throw error 
+    throw error // Rethrow the error to be caught by the caller
   }
 }
 
-
+/**
+ * Fetch a single blog post by its ID
+ * @param {number} id The ID of the blog post to fetch
+ * @returns {Promise<Object>} A promise that resolves to the blog post object
+ */
 const findById = async (id) => {
   try {
     const response = await fetch(`${BASE_URL}/${id}`)
@@ -24,7 +33,7 @@ const findById = async (id) => {
     return await response.json()
   } catch (error) {
     console.error(error)
-    throw error 
+    throw error // Rethrow the error to be caught by the caller
   }
 }
 
